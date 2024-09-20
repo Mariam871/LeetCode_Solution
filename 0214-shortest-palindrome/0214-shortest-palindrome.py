15e6758db6c343a221a7e22546130a9521ae35b8
Mariam Ashraf <114966897+Mariam871@users.noreply.github.com>
2024-09-21 02:32:38 +0500

    Time: 63 ms (69.51%), Space: 16.7 MB (92.07%) - LeetHub

--- 0214-shortest-palindrome/0214-shortest-palindrome.py
+++ 0214-shortest-palindrome/0214-shortest-palindrome.py
@@ -2,16 +2,9 @@ class Solution:
     def shortestPalindrome(self, s: str) -> str:
         if not s:
             return s
-        
-        # Reverse the string
         rev_s = s[::-1]
-        
-        # Check the largest palindromic prefix
         for i in range(len(s), 0, -1):
             if s[:i] == rev_s[len(s)-i:]:
-                # Found the longest palindromic prefix
                 break
-        
-        # Reverse the suffix and add it to the front
         suffix = s[i:]
         return suffix[::-1] + s
