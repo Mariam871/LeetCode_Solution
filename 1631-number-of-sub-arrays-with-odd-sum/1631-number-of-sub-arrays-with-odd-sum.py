b1a1f34f05b2d59ab02fd032f0defefab39f4649
Mariam Ashraf <114966897+Mariam871@users.noreply.github.com>
2025-02-25 21:21:08 +0500

    Time: 63 ms (84.95%), Space: 21.7 MB (99.19%) - LeetHub

---
+++ 1631-number-of-sub-arrays-with-odd-sum/1631-number-of-sub-arrays-with-odd-sum.py
@@ -0,0 +1,21 @@
+from typing import List
+
+class Solution:
+    def numOfSubarrays(self, arr: List[int]) -> int:
+        MOD = 10**9 + 7
+        odd_count = 0
+        even_count = 1  # To consider subarrays starting from index 0
+        current_sum = 0
+        result = 0
+
+        for num in arr:
+            current_sum += num
+            
+            if current_sum % 2 == 0:
+                result += odd_count
+                even_count += 1
+            else:
+                result += even_count
+                odd_count += 1
+
+        return result % MOD
