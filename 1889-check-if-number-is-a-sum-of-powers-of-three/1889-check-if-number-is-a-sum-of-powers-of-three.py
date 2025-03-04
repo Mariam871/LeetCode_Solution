213a47c13b297cfa3868fa83099b2553251c2cd8
Mariam Ashraf <114966897+Mariam871@users.noreply.github.com>
2025-03-04 20:26:28 +0500

    Time: 0 ms (100%), Space: 17.8 MB (38.84%) - LeetHub

---
+++ 1889-check-if-number-is-a-sum-of-powers-of-three/1889-check-if-number-is-a-sum-of-powers-of-three.py
@@ -0,0 +1,8 @@
+class Solution:
+    def checkPowersOfThree(self, n: int) -> bool:
+        while n > 0:
+            if n % 3 == 2:  # If remainder is 2, it's not possible to represent n using distinct powers of 3
+                return False
+            n //= 3  # Reduce n by dividing by 3
+        return True
+
