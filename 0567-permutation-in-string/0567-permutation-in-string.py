11582bbde0d2938198ac6bd3a389047a56a4639e
Mariam Ashraf <114966897+Mariam871@users.noreply.github.com>
2025-07-17 20:52:22 +0500

    Time: 11 ms (97.3%), Space: 17.8 MB (63.1%) - LeetHub

--- 0567-permutation-in-string/0567-permutation-in-string.py
+++ 0567-permutation-in-string/0567-permutation-in-string.py
@@ -3,32 +3,21 @@ class Solution:
         if len(s1) > len(s2):
             return False
 
-        s1Count, s2Count = [0] * 26, [0] * 26
+        s1_count = [0] * 26
+        s2_count = [0] * 26
+
         for i in range(len(s1)):
-            s1Count[ord(s1[i]) - ord("a")] += 1
-            s2Count[ord(s2[i]) - ord("a")] += 1
+            s1_count[ord(s1[i]) - ord('a')] += 1
+            s2_count[ord(s2[i]) - ord('a')] += 1
 
-        matches = 0
-        for i in range(26):
-            matches += 1 if s1Count[i] == s2Count[i] else 0
+        if s1_count == s2_count:
+            return True
 
-        l = 0
-        for r in range(len(s1), len(s2)):
-            if matches == 26:
+        for i in range(len(s1), len(s2)):
+            s2_count[ord(s2[i]) - ord('a')] += 1
+            s2_count[ord(s2[i - len(s1)]) - ord('a')] -= 1
+
+            if s1_count == s2_count:
                 return True
 
-            index = ord(s2[r]) - ord("a")
-            s2Count[index] += 1
-            if s1Count[index] == s2Count[index]:
-                matches += 1
-            elif s1Count[index] + 1 == s2Count[index]:
-                matches -= 1
-
-            index = ord(s2[l]) - ord("a")
-            s2Count[index] -= 1
-            if s1Count[index] == s2Count[index]:
-                matches += 1
-            elif s1Count[index] - 1 == s2Count[index]:
-                matches -= 1
-            l += 1
-        return matches == 26
+        return False
